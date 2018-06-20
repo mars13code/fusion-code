3d107a861a462b13bceae3669ebc9f0f5e17a0e7
test1 <test1@mail.me>
2018-06-20 19:45:35 +0200

    test

--- git-auto.bat
+++ git-auto.bat
@@ -1,3 +1,8 @@
+
+cd ../
+
+cd fusion-code
+
 git status
 
 git add -A 
@@ -7,3 +12,45 @@ git commit -a -m "test"
 git push
 
 git status
+
+cd ../
+
+cd fusion-site
+
+git status
+
+git add -A 
+
+git commit -a -m "test"
+
+git push
+
+git status
+
+cd ../
+cd fusion-plugin
+
+git status
+
+git add -A 
+
+git commit -a -m "test"
+
+git push
+
+git status
+
+cd ../
+cd fusion-theme
+
+git status
+
+git add -A 
+
+git commit -a -m "test"
+
+git push
+
+git status
+
+cd ../
